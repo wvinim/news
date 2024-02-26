bd615ddb61efb0d35fc8bf1c706110d28af80b3c
Vine <wvinim@msn.com>
2024-02-26 18:13:47 -0300

    feat: add NewsDetails component

---
+++ frontend/src/components/NewsDetails.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Grid } from "@mui/material";
+import Header from "./Header";
+import NewsView from "./NewsView";
+
+const NewsDetails = () => {
+  const navigate = useNavigate();
+  const [news, setNews] = useState({});
+  const { url } = useParams();
+  const basePathUrl = import.meta.env.VITE_BASE_API_URL;
+
+  const breakUrl = url.split("-");
+
+  const id = breakUrl[breakUrl.length - 1];
+
+  useEffect(() => {
+    axios
+      .get(`${basePathUrl}/${id}`)
+      .then((response) => {
+        setNews(response.data);
+      })
+      .catch((error) => {
+        navigate("/");
+        console.error("Error fetching news:", error);
+      });
+  }, []);
+
+  return (
+    <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+      <Header />
+      <Grid item xs={10}>
+        <NewsView article={news} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default NewsDetails;

933995b35016e89e481c5e97979f47dbb15679af
Vine <wvinim@msn.com>
2024-02-26 15:23:28 -0300

    fix: add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const { setUpDatabase } = require("./loaders/db");
@@ -8,6 +9,7 @@ const { API_PORT } = require("./config");
 const app = express();
 const routes = require("./api/routes");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", routes);
 

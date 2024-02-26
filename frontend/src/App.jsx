fe0112c2dba6caffef572918ddcabdbd4323bcd3
Vine <wvinim@msn.com>
2024-02-26 18:15:22 -0300

    feat: add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NewsList from "./components/NewsList";
+import NewsDetails from "./components/NewsDetails";
 import { NewsProvider } from "./contexts/NewsContext";
 
 function App() {
   return (
     <NewsProvider>
-      <div className="App">
-        <NewsList />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<NewsList />}></Route>
+          <Route path="/news/:url" element={<NewsDetails />}></Route>
+        </Routes>
+      </Router>
     </NewsProvider>
   );
 }
--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -1,29 +1,39 @@
+import { Link } from "react-router-dom";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const NewsCard = ({ article }) => {
   return (
-    <Card
-      sx={{
-        border: "none",
-        boxShadow: "none",
-        marginLeft: "20px",
-        marginRight: "20px",
-      }}
+    <Link
+      to={`/news/${article.titulo
+        .toLowerCase()
+        .replace(/[^a-z\s]/g, "")
+        .trim()
+        .replace(/\s+/g, "-")}-${article.id}`}
+      style={{ textDecoration: "none" }}
     >
-      <CardMedia
-        component="img"
-        image={article.thumbnail}
-        alt={article.titulo}
-      />
-      <CardContent>
-        <Typography variant="caption" component="div">
-          {article.chapeu}
-        </Typography>
-        <Typography variant="h6" component="div">
-          {article.titulo}
-        </Typography>
-      </CardContent>
-    </Card>
+      <Card
+        sx={{
+          border: "none",
+          boxShadow: "none",
+          marginLeft: "20px",
+          marginRight: "20px",
+        }}
+      >
+        <CardMedia
+          component="img"
+          image={article.thumbnail}
+          alt={article.titulo}
+        />
+        <CardContent>
+          <Typography variant="caption" component="div">
+            {article.chapeu}
+          </Typography>
+          <Typography variant="h6" component="div">
+            {article.titulo}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 

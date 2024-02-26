dc734ea9ab6c0c78204b2a24ae70d8dbb5a0c948
Vine <wvinim@msn.com>
2024-02-26 16:05:25 -0300

    refactor: newscard component styles

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -2,13 +2,23 @@ import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const NewsCard = ({ article }) => {
   return (
-    <Card sx={{ border: "none", boxShadow: "none" }}>
+    <Card
+      sx={{
+        border: "none",
+        boxShadow: "none",
+        marginLeft: "20px",
+        marginRight: "20px",
+      }}
+    >
       <CardMedia
         component="img"
         image={article.thumbnail}
         alt={article.titulo}
       />
       <CardContent>
+        <Typography variant="caption" component="div">
+          {article.chapeu}
+        </Typography>
         <Typography variant="h6" component="div">
           {article.titulo}
         </Typography>

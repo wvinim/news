cc75cbc0bfa5547e27ed8e2c9e474cb6bfe0e30b
Vine <wvinim@msn.com>
2024-02-26 20:12:53 -0300

    fix: redirect undefined route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import NewsList from "./ui/pages/NewsList";
 import NewsDetails from "./ui/pages/NewsDetails";
 import { NewsProvider } from "./contexts/NewsContext";
@@ -9,8 +14,9 @@ function App() {
     <NewsProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<NewsList />}></Route>
-          <Route path="/news/:url" element={<NewsDetails />}></Route>
+          <Route path="/" element={<NewsList />} />
+          <Route path="/news/:url" element={<NewsDetails />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </NewsProvider>
